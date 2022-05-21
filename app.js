398be51682b5d75079dda74b2f3d84858ebf4560
Bolaji Okebanjo <bokebanjo@yahoo.com>
2022-05-21 19:53:19 +0100

    Update app.js
    git add app.js
    git commit -m "Full pipeline test"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I love coding always!'));
+app.get('/', (req, res) => res.send('I love DevOps always!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
